e9931817ecd822b381b2ee290999c0ed4ed0e200
bbaecco <46706915+bbaecco@users.noreply.github.com>
2024-01-08 11:35:02 +0900

    initial commit

---
+++ src/main/java/practice/roulettee/RouletteeApplication.java
@@ -0,0 +1,13 @@
+package practice.roulettee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RouletteeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RouletteeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/practice/roulettee/RouletteeApplicationTests.java
@@ -0,0 +1,13 @@
+package practice.roulettee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RouletteeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
